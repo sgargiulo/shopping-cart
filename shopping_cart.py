726cb440d5742d9ffa3c34786956a3e120ac3f3a
Sean Gargiulo <spg363@stern.nyu.edu>
2019-06-11 15:33:34 -0400

    displaying inputs and total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -35,21 +35,33 @@ products = [
 #
 
 total_price = 0
+selected_ids = []
+
 
 while True:  #this will loop the program
     selected_id = input("Please input a product identifier: ")  #this is a string 
     if selected_id == "DONE":
         break   #this ends the loop
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
 
 
 #
 # INFO DISPLAY
 #
 
+
+
+#print(selected_ids)
+
+for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 print("TOTAL PRICE: " + str(total_price))
